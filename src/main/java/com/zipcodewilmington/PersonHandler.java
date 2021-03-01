128b4631c24ad31986d2874480f350aa58b5bde5
xiong <xiong@zipcodes-MBP-8.fios-router.home>
2021-03-01 15:16:40 -0500

    wrote loops to pass each test

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -12,6 +12,8 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
+        StringBuilder newPerson = new StringBuilder();
+        int counter = 0;
         // create a `counter`
         // while `counter` is less than length of array
             // begin loop
@@ -21,6 +23,11 @@ public class PersonHandler {
                 // append `stringRepresentation` to `result` variable
 
             // end loop
+        while (counter < this.personArray.length) {
+            newPerson.append(this.personArray[counter]);
+            counter++;
+        }
+        result = newPerson.toString();
         return result;
     }
 
@@ -28,6 +35,7 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
+        StringBuilder newPerson = new StringBuilder();
         // identify initial value
         // identify terminal condition
         // identify increment
@@ -38,7 +46,10 @@ public class PersonHandler {
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
-
+        for (int i = 0; i < this.personArray.length; i++) {
+            newPerson.append(this.personArray[i]);
+        }
+        result = newPerson.toString();
         return result;
     }
 
@@ -46,6 +57,7 @@ public class PersonHandler {
 
     public String forEachLoop() {
         String result = "";
+        StringBuilder peeps = new StringBuilder();
         // identify array's type
         // identify array's variable-name
 
@@ -54,7 +66,11 @@ public class PersonHandler {
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
+        for(Person newPerson:this.personArray) {
+            peeps.append(newPerson);
+        }
 
+        result = peeps.toString();
         return result;
     }
 
